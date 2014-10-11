ff7c1d15aeed9514d41596319c924506061bc256
Matthew Franglen <matthew@franglen.org>
2014-10-11 01:10:40 +0100

    Handle transferring goods to shop
    Deliveries between the warehouse and shop are also covered apparently.

--- src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/good/GoodPlugin.java
+++ src/main/java/xyz/rjs/brandwatch/supermarkets/logistics/plugins/good/GoodPlugin.java
@@ -15,6 +15,7 @@ import xyz.rjs.brandwatch.supermarkets.model.events.ClockTick;
 import xyz.rjs.brandwatch.supermarkets.model.events.Customer;
 import xyz.rjs.brandwatch.supermarkets.model.events.Order;
 import xyz.rjs.brandwatch.supermarkets.model.events.PriceList;
+import xyz.rjs.brandwatch.supermarkets.sim.Shop;
 import xyz.rjs.brandwatch.supermarkets.sim.Warehouse;
 
 import com.google.common.eventbus.Subscribe;
@@ -40,17 +41,11 @@ import com.google.common.eventbus.Subscribe;
  * stock.
  * 
  * This plugin always assumes that the current price is the average price for
- * the forseeable future. It does not make decisions based on the price. This is
- * reasonable because the price always has an even chance of going up or down
- * (except when it is at rock bottom, when it has an increased chance of staying
- * the same).
+ * the forseeable future. It will check that the price permits a profit, but
+ * otherwise will purchase at any price.
  * 
- * NOTE: Review this. The sale price is 10, so if the buy price meets or exceeds
- * that no purchases should be made.
- * 
- * NOTE: The balance is declared on the first tick. However the Balance object
- * has no accessor so it is unsporting to read it. The string form could be
- * easily parsed.
+ * The plugin will ensure that the shop always has enough stock to cover a
+ * single tick of demand.
  * 
  * In general this is a very conservative approach to take.
  * 
@@ -62,10 +57,10 @@ public class GoodPlugin extends AbstractPlugin {
 	private static final Logger logger = LoggerFactory.getLogger(GoodPlugin.class);
 
 	/**
-	 * These are the trades that are issued by the GATHER_DATA state. 
-	 * The total of 78 is slightly under the lowest starting balance of 85.
+	 * These are the trades that are issued by the GATHER_DATA state. The total
+	 * of 78 is slightly under the lowest starting balance of 85.
 	 */
-	private static final int[] STARTING_TRADES = new int[] {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	private static final int[] STARTING_TRADES = new int[] { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 	/**
 	 * This is the buy price at which it becomes unprofitable to buy any stock.
 	 */
@@ -73,6 +68,9 @@ public class GoodPlugin extends AbstractPlugin {
 
 	@Autowired
 	private Warehouse warehouse;
+
+	@Autowired
+	private Shop shop;
 
 	/**
 	 * The current state of the plugin.
@@ -115,6 +113,12 @@ public class GoodPlugin extends AbstractPlugin {
 
 	@Subscribe
 	public void arrivalListener(ArrivalNotification arrival) {
+		// Only arrivals at the Warehouse need to be tracked.
+		// Warehouse to Shop is instant (see WarehouseManagementSystem).
+		if (arrival.getPlace() != warehouse) {
+			return;
+		}
+
 		final int amount = arrival.getAmount();
 		Optional<PendingOrder> order = orders.stream().filter(o -> o.getVolume() == amount).findFirst();
 
@@ -146,19 +150,32 @@ public class GoodPlugin extends AbstractPlugin {
 	public void tickListener(ClockTick tick) {
 		this.tick = tick.getTick(); // saved so that arrivals can be tracked.
 		state.tickListener(this, tick);
+		stockShop();
 
 		logger.info(String.format(
-			"\nTICK: Delivery Time (%.2f) Purchase per Tick (%.2f) Current Stock (%d) Total Stock (%d) Required Stock (%d)",
-			deliveries.confidence(),
-			purchases.confidence(this.tick),
-			warehouse.getStock(),
-			getTotalStock(),
-			getRequiredStock()
-		));
+				"\nTICK: Delivery Time (%.2f) Purchase per Tick (%.2f) Shop Stock (%d) Warehouse Stock (%d) Total Stock (%d) Required Stock (%d)",
+				deliveries.confidence(), purchases.confidence(this.tick), shop.getStock(), warehouse.getStock(), getTotalStock(), getRequiredStock()));
+	}
+
+	/**
+	 * This ensures that the shop has at least enough stock for a single tick of
+	 * sales.
+	 */
+	private void stockShop() {
+		// The calculation of p here does *not* use the tick count because this
+		// is a calculation that only considers ticks where there was a sale.
+		int p = (int) Math.ceil(purchases.confidence());
+
+		if (shop.getStock() < p && warehouse.getStock() > 0) {
+			int volume = Math.min(p - shop.getStock(), warehouse.getStock());
+			shop.addStock(volume);
+			warehouse.setStock(warehouse.getStock() - volume);
+		}
 	}
 
 	/**
 	 * Creates and tracks the order.
+	 * 
 	 * @param volume
 	 */
 	private void placeOrder(int volume) {
@@ -170,22 +187,25 @@ public class GoodPlugin extends AbstractPlugin {
 
 	/**
 	 * Get the current warehouse stock and all outstanding deliveries.
+	 * 
 	 * @return
 	 */
 	private int getTotalStock() {
-		return warehouse.getStock() + orders.stream().mapToInt(order -> order.getVolume()).sum();
+		return shop.getStock() + warehouse.getStock() + orders.stream().mapToInt(order -> order.getVolume()).sum();
 	}
 
 	/**
 	 * Get the required stock to survive for the length of an average delivery.
+	 * 
 	 * @return
 	 */
 	private int getRequiredStock() {
-		return (int)Math.ceil((deliveries.confidence() * purchases.confidence(tick)));
+		return (int) Math.ceil((deliveries.confidence() * purchases.confidence(tick)));
 	}
 
 	/**
 	 * Transition the plugin through the different GoodPlugin.STATE states.
+	 * 
 	 * @param state
 	 */
 	private void setState(STATE state) {
@@ -195,13 +215,15 @@ public class GoodPlugin extends AbstractPlugin {
 	}
 
 	/**
-	 * Provides the behaviours associated with the different states the GoodPlugin can be in.
+	 * Provides the behaviours associated with the different states the
+	 * GoodPlugin can be in.
 	 * 
 	 * @author matthew
 	 */
 	private enum STATE {
 		/**
-		 * The START state waits for the price to become available. Once the price is available trading can begin.
+		 * The START state waits for the price to become available. Once the
+		 * price is available trading can begin.
 		 */
 		START() {
 
@@ -211,17 +233,17 @@ public class GoodPlugin extends AbstractPlugin {
 			}
 		},
 		/**
-		 * The GATHER_DATA state issues a set of initial trades to seed the 
+		 * The GATHER_DATA state issues a set of initial trades to seed the
 		 */
 		GATHER_DATA() {
 
 			@Override
 			void tickListener(GoodPlugin plugin, ClockTick tick) {
-				if (plugin.startingTradesIndex < STARTING_TRADES.length) {
+				if (plugin.startingTradesIndex < STARTING_TRADES.length && plugin.price < PRICE_LIMIT) {
 					plugin.placeOrder(STARTING_TRADES[plugin.startingTradesIndex]);
 					plugin.startingTradesIndex++;
 				}
-				if (! (plugin.deliveries.isEmpty() || plugin.purchases.isEmpty())) {
+				if (!(plugin.deliveries.isEmpty() || plugin.purchases.isEmpty())) {
 					plugin.setState(plugin.price >= PRICE_LIMIT ? OVERPRICED : TRADE);
 				}
 			}
@@ -252,10 +274,13 @@ public class GoodPlugin extends AbstractPlugin {
 					plugin.setState(TRADE);
 				}
 			}
-			
+
 		};
 
-		void tickListener(GoodPlugin plugin, ClockTick tick) {}
-		void priceListener(GoodPlugin plugin, PriceList priceList) {}
+		void tickListener(GoodPlugin plugin, ClockTick tick) {
+		}
+
+		void priceListener(GoodPlugin plugin, PriceList priceList) {
+		}
 	}
 }
